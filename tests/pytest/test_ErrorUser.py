cec15dd56dc71ac67b7a2b8527471b26541f162d
jbeaver-abellera <abellera.beaver@gmail.com>
2024-10-21 01:11:57 +0800

    Update test_ErrorUser.py
    remote commit x = hello world

--- tests/pytest/test_ErrorUser.py
+++ tests/pytest/test_ErrorUser.py
@@ -28,7 +28,7 @@ def test_error_user(setup_teardown):
     error_list = []
     test_case = "error_user"
     loginPage = setup_teardown
-    x = 888
+    x = 'hello world'
     
     '''
     ## Check Login Functionality
