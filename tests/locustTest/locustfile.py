5a3e9cdcec6a52e5706cdffb6f9cfefa03af6beb
Joshua Beaver Abellera <joshua.abellera@cambridge.org>
2024-10-21 00:47:01 +0800

    idk what this is but lets just proceed

--- tests/locustTest/locustfile.py
+++ tests/locustTest/locustfile.py
@@ -1,14 +1,14 @@
-import time
-from locust import HttpUser, task, between
-from utils.Constants import Constants
-
-class loadTest(HttpUser):
-    wait_time = between(1, 5)
-    
-    @task
-    def swaglabs(self):
-        self.client.get(url= Constants.WEBSITE_URL, name="swaglabs")
-        
-    @task
-    def the_internet(self):
-        self.client.get(url="https://the-internet.herokuapp.com/", name="the-internet")
+# import time
+# from locust import HttpUser, task, between
+# from utils.Constants import Constants
+#
+# class loadTest(HttpUser):
+#     wait_time = between(1, 5)
+#
+#     @task
+#     def swaglabs(self):
+#         self.client.get(url= Constants.WEBSITE_URL, name="swaglabs")
+#
+#     @task
+#     def the_internet(self):
+#         self.client.get(url="https://the-internet.herokuapp.com/", name="the-internet")
